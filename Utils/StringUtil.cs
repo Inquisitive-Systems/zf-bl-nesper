61b0fea66da1bc10efdeb099c24c8f3fb1ce4bc6
Matthew Summers <m.summers@zonefox.com>
2018-02-09 15:15:49 +0000

    Add InRange extension method for use in EPL

--- Utils/StringUtil.cs
+++ Utils/StringUtil.cs
@@ -1,9 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.Globalization;
+using System.IO;
 
 namespace ZF.BL.Nesper.Utils
 {
     public class StringUtil
     {
+        /// <summary>
+        /// Represents the inclusivity/exclusivity state of a search query
+        /// </summary>
+        public enum RangeOpenness
+        {
+            /// <summary>
+            /// Query which includes its boundary elements
+            /// </summary>
+            Inclusive,
+            /// <summary>
+            /// Query which excludes its boundary elements
+            /// </summary>
+            Exclusive
+        }
+
         public static string TrimEnd(string input, string trimSting)
         {
             return input.TrimEnd(trimSting.ToCharArray());
@@ -29,6 +46,36 @@ namespace ZF.BL.Nesper.Utils
             {
                 return false;
             }
+        }
+
+        public static bool InRange(long val, string from, string to, string openness)
+        {
+            var fromL = long.Parse(from, CultureInfo.InvariantCulture);
+            var toL = long.Parse(to, CultureInfo.InvariantCulture);
+            var open = (RangeOpenness)Enum.Parse(typeof(RangeOpenness), openness, true);
+
+            if (open == RangeOpenness.Inclusive)
+            {
+                return fromL <= val && val <= toL;
+            }
+
+            //Exclusive
+            return fromL < val && val < toL;
+        }
+
+        public static bool InRange(DateTime val, string from, string to, string openness)
+        {
+            var fromDate = DateTime.ParseExact(from, "o", CultureInfo.InvariantCulture);
+            var toDate = DateTime.ParseExact(to, "o", CultureInfo.InvariantCulture);
+            var open = (RangeOpenness) Enum.Parse(typeof(RangeOpenness), openness, true);
+
+            if (open == RangeOpenness.Inclusive)
+            {
+                return fromDate <= val && val <= toDate;
+            }
+
+            //Exclusive
+            return fromDate < val && val  < toDate;
         }
     }
 }
