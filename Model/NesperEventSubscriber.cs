584e5c1c5b726fb156423ec612e44af2435b6f78
Oleksii Mandrychenko <o.mandrychenko@zonefox.com>
2018-09-10 16:30:14 +0100

    Code review changes and tests

--- Model/NesperEventSubscriber.cs
+++ Model/NesperEventSubscriber.cs
@@ -10,7 +10,7 @@ using ZF.BL.Nesper.Utils;
 
 namespace ZF.BL.Nesper.Model
 {
-    internal sealed class NesperEventSubscriber
+    public sealed class NesperEventSubscriber
     {
         // Assume a max ceiling for JSON serrialised alert - 5 MB 
         private const int MaxAlertSizeInBytes = 5 * 1024 * 1024;
@@ -37,7 +37,7 @@ namespace ZF.BL.Nesper.Model
                 throw new ArgumentNullException(nameof(e), "e.NewEvents");
             if (e.NewEvents.Length == 0)
                 throw new InvalidOperationException("Engine event fired but no data provided about the event");
-            
+
             var events = ExtractEvents(e);
 
             if (events.Count == 0)
@@ -46,13 +46,11 @@ namespace ZF.BL.Nesper.Model
             SendChunkedAlerts(events);
         }
 
-        // Chunk alerts based on the size of events
-        // Send a new alert each time when the output message size is greater than N MB
-        // For perfomrance reasons we only estimate the size of result alert 
-        private void SendChunkedAlerts(List<ActivityEvent> events)
+        public List<List<ActivityEvent>> ChunkBy(int sizeInBytes, List<ActivityEvent> events)
         {
-            int estimatedAlertSizeInBytes = 0; // temp variable to hold current estimated message size
+            int estimatedAlertSizeInBytes = 0; // temp variable to hold current estimated size
             var tmpEvents = new List<ActivityEvent>(); // temp list to store copies of events during multiple iterations
+            var result = new List<List<ActivityEvent>>(); // list we are going to return
 
             // scan all events
             foreach (var ev in events)
@@ -61,13 +59,10 @@ namespace ZF.BL.Nesper.Model
                 estimatedAlertSizeInBytes += EstimateEventSizeInBytes(ev);
                 tmpEvents.Add(ev);
 
-                // have we reached max alert size limit?
-                if (estimatedAlertSizeInBytes > MaxAlertSizeInBytes)
+                // have we reached max size limit?
+                if (estimatedAlertSizeInBytes > sizeInBytes)
                 {
-                    // yes, let's send this 
-                    SendAlert(tmpEvents, estimatedAlertSizeInBytes);
-
-                    // clear up temp state
+                    result.Add(tmpEvents);
                     tmpEvents.Clear();
                     estimatedAlertSizeInBytes = 0;
                 }
@@ -75,75 +70,96 @@ namespace ZF.BL.Nesper.Model
 
             // check if we have any leftover events for which we did not send an alert 
             if (tmpEvents.Count > 0)
-                SendAlert(tmpEvents, estimatedAlertSizeInBytes);
+                result.Add(tmpEvents);
+
+            return result;
+        }
+
+        // Chunk alerts based on the size of events
+        // Send a new alert each time when the output message size is greater than N MB
+        // For perfomrance reasons we only estimate the size of result alert 
+        public void SendChunkedAlerts(List<ActivityEvent> events)
+        {
+            var eventBatches = ChunkBy(MaxAlertSizeInBytes, events);
+            foreach (var batch in eventBatches)
+                SendAlert(batch);
         }
 
         // Add alert to a shared memory buffer
-        private void SendAlert(List<ActivityEvent> tmpEvents, int estimatedAlertSizeInBytes)
+        public void SendAlert(List<ActivityEvent> tmpEvents)
         {
             var alert = new ActivityAlert(_ruleId, DateTime.UtcNow, tmpEvents.ToArray());
             var offset = new DateTimeOffset(DateTime.UtcNow);
-
             string logMessage = $"New alert: {offset:dd-MM-yyyy HH:mm:ss}, rule id: {alert.RuleId}, " +
-                                $"user {alert.Events[0].User}, {tmpEvents.Count} events " +
-                                $"of estimates size {estimatedAlertSizeInBytes} bytes";
+                                $"user {alert.Events[0].User}, {tmpEvents.Count} events ";
             _alertLog.Warn(logMessage);
-
             NesperPerformance.TotalAlerts++;
             _alertBuffer.Add(alert);
         }
 
         // Produce a quick best guess of event size in bytes
         // Current size is a lower bound for json object required to represent serialised event
-        private int EstimateEventSizeInBytes(ActivityEvent ev)
+        public int EstimateEventSizeInBytes(ActivityEvent ev)
         {
             // assume there's more things in the event, such as
             // size of "dd-MM-yyyyTHH:mm:ss+01:00" datetime string,
             // other json object properties and potentially network metadata 
-            const int supplement = 60; 
+            int estimatedSize = 60;
 
-            int estimatedSize = supplement + ev.Machine.Length + ev.User.Length + ev.Application.Length + ev.Activity.Length + ev.Resource.Length;
-
+            if (!string.IsNullOrEmpty(ev.Machine))
+                estimatedSize += ev.Machine.Length;
+            if (!string.IsNullOrEmpty(ev.User))
+                estimatedSize += ev.User.Length;
+            if (!string.IsNullOrEmpty(ev.Application))
+                estimatedSize += ev.Application.Length;
+            if (!string.IsNullOrEmpty(ev.Activity))
+                estimatedSize += ev.Activity.Length;
+            if (!string.IsNullOrEmpty(ev.Resource))
+                estimatedSize += ev.Resource.Length;
+            
             // every character takes at least 2 bytes in Unicode which we use in C#
-            estimatedSize *= 2; 
+            estimatedSize *= 2;
 
             return estimatedSize;
         }
 
-        private List<ActivityEvent> ExtractEvents(UpdateEventArgs e)
+        public List<ActivityEvent> ExtractEvents(UpdateEventArgs e)
         {
             var list = new List<ActivityEvent>();
 
-            // process dictionary of events
+            if (e.NewEvents == null || e.NewEvents.Length == 0)
+                return list;
+
+            // process a dictionary of events
             if (e.NewEvents.First().GetType() == typeof(MapEventBean))
             {
                 foreach (var mapBean in e.NewEvents)
-                foreach (var kv in ((IDictionary<string, object>) mapBean.Underlying))
-                {
-                    // kv.Value can be 
-                    //   - Many events, e.g. EventBean[]
-                    //   - One events, e.g. BeanEventBean
-                    //   - Null, e.g. when we looking for a missing event like A and NOT B in outer join
-
-                    if (kv.Value == null)
-                        continue; // not interesed in the missing event, carry on looping
-
-                    // many events, e.g. when we do "select ev1, ev2, ev3 ... "
-                    if (kv.Value is EventBean[] beans)
+                    foreach (var kv in ((IDictionary<string, object>)mapBean.Underlying))
                     {
-                        list.AddRange(beans.Select(x => (ActivityEvent) x.Underlying));
-                    }
+                        // kv.Value is an object and it can be 
+                        //   - Many events, e.g. EventBean[]
+                        //   - One events, e.g. BeanEventBean
+                        //   - Null, e.g. when we looking for a missing event like A and NOT B in outer join
 
-                    // one event, e.g. when we do "select ev1 from ..."
-                    if (kv.Value is BeanEventBean bean)
-                    {
-                        list.Add((ActivityEvent) bean.Underlying);
+                        // not interested in the missing event, carry on looping
+                        if (kv.Value == null)
+                            continue;
+
+                        // many events, e.g. when we do "select ev1, ev2, ev3 ... "
+                        if (kv.Value is EventBean[] beans)
+                            list.AddRange(beans.Select(x => (ActivityEvent)x.Underlying));
+
+                        // one event, e.g. when we do "select ev1 from ..."
+                        if (kv.Value is BeanEventBean bean)
+                            list.Add((ActivityEvent)bean.Underlying);
                     }
-                }
             }
-            else // process events
+            else 
             {
-                list = (from row in e.NewEvents select (ActivityEvent) row.Underlying).ToList();
+                list = (from row in e.NewEvents
+                        where row.Underlying != null && 
+                              row.Underlying.GetType() == typeof(ActivityEvent)
+                        select (ActivityEvent)row.Underlying).ToList();
             }
 
             return list;
