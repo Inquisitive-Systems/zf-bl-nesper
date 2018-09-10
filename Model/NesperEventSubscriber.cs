3780be67c314a6e7fa4b3c84fcb33de8ae8a6b42
Oleksii Mandrychenko <o.mandrychenko@zonefox.com>
2018-09-10 10:51:21 +0100

    Chunk events in alert based on size of the event

--- Model/NesperEventSubscriber.cs
+++ Model/NesperEventSubscriber.cs
@@ -12,8 +12,10 @@ namespace ZF.BL.Nesper.Model
 {
     internal sealed class NesperEventSubscriber
     {
-        private readonly ILog _alertLog = LogManager.GetLogger(BlLog.AlertsLog);
+        // Assume a max ceiling for JSON serrialised alert - 5 MB 
+        private const int MaxAlertSizeInBytes = 5 * 1024 * 1024;
 
+        private readonly ILog _alertLog = LogManager.GetLogger(BlLog.AlertsLog);
         private readonly BlockingCollection<ActivityAlert> _alertBuffer;
         private readonly string _ruleId;
 
@@ -25,9 +27,9 @@ namespace ZF.BL.Nesper.Model
 
         /// <summary>
         ///     This method must have this specific signature,
-        ///     otherwise engine wouldn't recognize it in runtime.
+        ///     otherwise Nesper engine wouldn't recognize it in runtime.
         ///     It is called by the engine subscribers and
-        ///     we retrieve our objects from the passed in args
+        ///     we retrieve our objects from the UpdateEventArgs object
         /// </summary>
         public void Update(object sender, UpdateEventArgs e)
         {
@@ -41,19 +43,71 @@ namespace ZF.BL.Nesper.Model
             if (events.Count == 0)
                 throw new InvalidOperationException("Failed to retrieve activity that triggered the alert");
 
-            // We need to chunk events, in case there's too many of them
-            // We saw one alert of 38 MB in production stored in ES
-            // Chunk events by 200, make a new alert for each batch of events
-            var chunkedEvents = events.ChunkBy(200);
+            SendChunkedAlerts(events);
+        }
 
-            foreach (var chunk in chunkedEvents)
+        // Chunk alerts based on the size of events
+        // Send a new alert each time when the output message size is greater than N MB
+        // For perfomrance reasons we only estimate the size of result alert 
+        private void SendChunkedAlerts(List<ActivityEvent> events)
+        {
+            int estimatedAlertSizeInBytes = 0; // temp variable to hold current estimated message size
+            var tmpEvents = new List<ActivityEvent>(); // temp list to store copies of events during multiple iterations
+
+            // scan all events
+            foreach (var ev in events)
             {
-                var alert = new ActivityAlert(_ruleId, DateTime.UtcNow, events.ToArray());
-                var offset = new DateTimeOffset(DateTime.UtcNow);
-                _alertLog.Warn($"New alert: {offset:dd-MM-yyyy HH:mm:ss}, rule id: {alert.RuleId}, user {alert.Events[0].User}, {chunk.Count} events");
-                NesperPerformance.TotalAlerts++;
-                _alertBuffer.Add(alert);
+                // calculate approximate size of this alert and add event to the temp list
+                estimatedAlertSizeInBytes += EstimateEventSizeInBytes(ev);
+                tmpEvents.Add(ev);
+
+                // have we reached max alert size limit?
+                if (estimatedAlertSizeInBytes > MaxAlertSizeInBytes)
+                {
+                    // yes, let's send this 
+                    SendAlert(tmpEvents, estimatedAlertSizeInBytes);
+
+                    // clear up temp state
+                    tmpEvents.Clear();
+                    estimatedAlertSizeInBytes = 0;
+                }
             }
+
+            // check if we have any leftover events for which we did not send an alert 
+            if (tmpEvents.Count > 0)
+                SendAlert(tmpEvents, estimatedAlertSizeInBytes);
+        }
+
+        // Add alert to a shared memory buffer
+        private void SendAlert(List<ActivityEvent> tmpEvents, int estimatedAlertSizeInBytes)
+        {
+            var alert = new ActivityAlert(_ruleId, DateTime.UtcNow, tmpEvents.ToArray());
+            var offset = new DateTimeOffset(DateTime.UtcNow);
+
+            string logMessage = $"New alert: {offset:dd-MM-yyyy HH:mm:ss}, rule id: {alert.RuleId}, " +
+                                $"user {alert.Events[0].User}, {tmpEvents.Count} events " +
+                                $"of estimates size {estimatedAlertSizeInBytes} bytes";
+            _alertLog.Warn(logMessage);
+
+            NesperPerformance.TotalAlerts++;
+            _alertBuffer.Add(alert);
+        }
+
+        // Produce a quick best guess of event size in bytes
+        // Current size is a lower bound for json object required to represent serialised event
+        private int EstimateEventSizeInBytes(ActivityEvent ev)
+        {
+            // assume there's more things in the event, such as
+            // size of "dd-MM-yyyyTHH:mm:ss+01:00" datetime string,
+            // other json object properties and potentially network metadata 
+            const int supplement = 60; 
+
+            int estimatedSize = supplement + ev.Machine.Length + ev.User.Length + ev.Application.Length + ev.Activity.Length + ev.Resource.Length;
+
+            // every character takes at least 2 bytes in Unicode which we use in C#
+            estimatedSize *= 2; 
+
+            return estimatedSize;
         }
 
         private List<ActivityEvent> ExtractEvents(UpdateEventArgs e)
